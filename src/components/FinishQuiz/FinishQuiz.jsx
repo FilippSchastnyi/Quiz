becbabfe8c8fdcc32f19256b5ecdb580aab07ca4
Филипп <filipp.schastny@ozitag.com>
2021-01-02 19:16:52 +0300

    the app is complite

---
+++ src/components/FinishQuiz/FinishQuiz.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import classes from './FinishQuiz.module.scss'
+
+const FinishQuiz = props => {
+    const successCount = Object.keys(props.results).reduce((total, key) =>{
+        if(props.results[key] === 'success'){
+            total++
+        }
+
+        return total
+    }, 0)
+
+    return (
+        <div className={classes.FinishedQuiz}>
+            <ul>
+                {props.quiz.map((item, index) => {
+                    const cls = [
+                        'fa',
+                        props.results[item.id] === 'false' ? 'fa-times' : 'fa-check',
+                        classes[props.results[item.id]]
+
+                    ]
+                    return(
+                        <li key={index}>
+                            <strong>{index + 1}</strong>
+                            {item.question}
+                            <i className={cls.join(' ')}/>
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <p>Правильно {successCount} из {props.quiz.length}</p>
+
+            <div>
+                <button onClick={props.onRetry}>Повторить</button>
+            </div>
+        </div>
+    )
+}
+
+export default FinishQuiz
